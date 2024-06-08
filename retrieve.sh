ed8b96a2bc899be36a0d0a5cd7f74970e8294b4b
impact112 <impact112@protonmail.com>
2024-06-09 01:15:51 +0300

    Add initial eww configs

--- retrieve.sh
+++ retrieve.sh
@@ -6,6 +6,5 @@ mkdir -p conf
 
 echo "Retrieving existing configuration files"
 cp -r "$HYPR_CONF_PATH" conf/
-cp -r "$WAYBAR_CONF_PATH" conf/
 cp -r "$FOOT_CONF_PATH" conf/
 cp -r "$EWW_CONF_PATH" conf/
